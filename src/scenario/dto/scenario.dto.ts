ba17988ea6c9fd97420bd65411186c30a9cecd5d
Sylvaner <sylvain.dangin@gmail.com>
2020-12-20 08:24:56 +0100

    Scenario IfThen fonctionnel

--- src/scenario/dto/scenario.dto.ts
+++ src/scenario/dto/scenario.dto.ts
@@ -4,15 +4,19 @@ export class CreateScenarioDTO {
   @ApiProperty()
   model: string;
   @ApiProperty()
+  name: string;
+  @ApiProperty()
   triggerDevice?: string;
   @ApiProperty()
   triggerCapability?: any;
   @ApiProperty()
+  triggerCondition: any;
+  @ApiProperty()
+  triggerValue: any;
+  @ApiProperty()
   actionDevice?: string;
   @ApiProperty()
   actionCapability?: any;
-  @ApiProperty()
-  triggerValue?: string;
   @ApiProperty()
   actionValue?: string;
 }
--- src/services/NodeRedConnector.ts
+++ src/services/NodeRedConnector.ts
@@ -1,152 +1,70 @@
 import axios from 'axios';
 import { CreateScenarioDTO } from 'src/scenario/dto/scenario.dto';
-import { Scenario } from 'src/scenario/interfaces/scenario.interface';
+import NodeRedModels from './NodeRedModels';
 
 const MQTT_NODE_ID = '77777777.777777';
 
-/*
-interface NodeRedGlobalFlow {
-  id: string,
-  configs: NodeRedNode[]
-}
-*/
-
 interface NodeRedNode {
-  id?: string,
-  name?: string,
-  type: string,
-  topic?: string,
-  label?: string,
-  disabled?: boolean,
-  datatype?: string,
-  template?: string,
-  field?: string,
-  fieldType?: string,
-  format?: string,
-  syntax?: string,
-  output?: string,
-  qos?: string,
-  broker?: string,
-  x?: number,
-  y?: number,
-  z?: string,
-  wires?: string[][]
+  id?: string;
+  name?: string;
+  type: string;
+  topic?: string;
+  label?: string;
+  disabled?: boolean;
+  datatype?: string;
+  template?: string;
+  field?: string;
+  fieldType?: string;
+  format?: string;
+  syntax?: string;
+  output?: string;
+  qos?: string;
+  broker?: string;
+  x?: number;
+  y?: number;
+  z?: string;
+  wires?: string[][];
 }
 
 export default class NodeRedConnector {
   private static instance: NodeRedConnector;
 
-  //  private mqttConfigNode: NodeRedNode = { type: '' };
+  private connected = false;
 
-  private connected: boolean = false;
+  private baseNodeRedServerUrl = '';
 
-  private baseNodeRedServerUrl: string = '';
-
-  private revisionCode: string = '';
-
+  // eslint-disable-next-line prettier/prettier
   private constructor() { }
 
   static getInstance(): NodeRedConnector {
     if (NodeRedConnector.instance === undefined) {
-      NodeRedConnector.instance = new NodeRedConnector()
+      NodeRedConnector.instance = new NodeRedConnector();
     }
     return NodeRedConnector.instance;
   }
 
-  public getRevisionCode(): Promise<string> {
-    return new Promise<string>((resolve, reject) => {
-      this.get('/flows').then(result => {
-        if (!result.hasOwnProperty('rev')) {
-          reject();
-        }
-        this.revisionCode = result.rev;
-        resolve(this.revisionCode);
-      }).catch(_ => {
-        reject();
-      });
+  private post(path: string, data: any): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      axios
+        .post(this.baseNodeRedServerUrl + path, data, {
+          headers: { 'Node-RED-API-Version': 'v2' },
+        })
+        .then((result) => {
+          if (result.status === 200) {
+            resolve(result.data);
+          } else {
+            reject();
+          }
+        })
+        .catch((err) => {
+          reject(err);
+        });
     });
   }
-
-  private get(path: string): Promise<any> {
-    return new Promise<object>((resolve, reject) => {
-      axios.get(this.baseNodeRedServerUrl + path, { headers: { 'Node-RED-API-Version': 'v2' } }).then((result) => {
-        if (result.status === 200) {
-          resolve(result.data)
-        }
-        else {
-          reject();
-        }
-      }).catch(err => {
-        reject(err);
-      })
-    })
-  }
-
-  public post(path: string, data: object): Promise<any> {
-    return new Promise<object>((resolve, reject) => {
-      axios.post(this.baseNodeRedServerUrl + path, data, { headers: { 'Node-RED-API-Version': 'v2' } }).then((result) => {
-        if (result.status === 200) {
-          resolve(result.data)
-        }
-        else {
-          reject();
-        }
-      }).catch(err => {
-        reject(err);
-      })
-    })
-  }
-
-  /*
-  readConfigFromGlobalFlow(globalFlowResponse: NodeRedGlobalFlow): boolean {
-    if (globalFlowResponse.hasOwnProperty('configs')) {
-      const filteredConfig = globalFlowResponse.configs.filter(config => config.id === MQTT_NODE_ID);
-      if (filteredConfig.length === 1) {
-        this.mqttConfigNode = filteredConfig[0];
-        return true;
-      }
-    }
-    return false;
-  }
-  */
 
   connect(nodeRedUrl: string) {
     this.baseNodeRedServerUrl = nodeRedUrl;
     this.connected = true;
-    /*
-    this.get('/flow/global').then(_ => {
-      //      if (this.readConfigFromGlobalFlow(result as NodeRedGlobalFlow)) {
-      this.connected = true;
-      this.saveFlow("TTT", [
-        {
-          name: 'Input',
-          type: 'mqtt in',
-          topic: 'phue/lights/2'
-        },
-        {
-          name: 'Action',
-          type: 'template',
-          template: '{\n  \'on\': true\n}',
-          output: 'json'
-        },
-        {
-          name: 'Output',
-          type: 'mqtt out',
-          topic: 'phue/lights/2'
-        },
-      ]);
-      */
-    /*
-    }
-    else {
-      throw new Error('Unable to read global config flow')
-    }
-    */
-    /*
-    }).catch(err => {
-      console.error(err)
-    })
-    */
   }
 
   isConnected(): boolean {
@@ -154,7 +72,7 @@ export default class NodeRedConnector {
   }
 
   /**
-   * From node-red source code
+   * Code from node-red source code
    */
   genId(): string {
     return (1 + Math.random() * 4294967295).toString(16);
@@ -165,8 +83,8 @@ export default class NodeRedConnector {
       id: this.genId(),
       label,
       type: 'tab',
-      disabled: false
-    }
+      disabled: false,
+    };
   }
 
   fillNodeData(rawNode: NodeRedNode): NodeRedNode {
@@ -189,83 +107,119 @@ export default class NodeRedConnector {
   }
 
   saveFlow(name: string, flow: NodeRedNode[]) {
-    const flowToSave: NodeRedNode[] = [];
-    //    const tabNode: NodeRedNode = this.createTabNode(flow.name);
-    //flowToSave.push(tabNode);
+    //const flowToSave: NodeRedNode[] = [];
+    /*
     const baseId = this.genId();
-    let addMqttConfig = false;
     for (const [index, rawNode] of flow.entries()) {
       rawNode.id = this.genId();
       rawNode.x = 150 + index * 150;
       rawNode.y = 200;
       rawNode.z = baseId;
-      if (rawNode.type === 'mqtt in' || rawNode.type === 'mqtt out') {
-        addMqttConfig = true;
-      }
       flowToSave.push(this.fillNodeData(rawNode));
     }
     // Add wires
     for (let i = 0; i < flowToSave.length - 1; ++i) {
-      flowToSave[i].wires = [
-        [
-          flowToSave[i + 1].id!
-        ]
-      ];
+      flowToSave[i].wires = [[flowToSave[i + 1].id!]];
     }
-    if (addMqttConfig) {
-      //      flowToSave.push(this.mqttConfigNode);
-    }
+    */
     const objectToSave = {
       id: this.genId(),
       label: name,
-      nodes: flowToSave,
-      configs: []
+      nodes: flow,
+      configs: [],
     };
-    this.post('/flow', objectToSave).then(result => {
-      console.log('a');
-    }).catch(err => {
-      //      console.log(err.response.data);
-      console.log(err);
-    });
-
-    /*
-    this.getRevisionCode().then(revisionCode => {
-      console.log(revisionCode);
-      console.log(flowToSave);
-      const objectToSave = {
-        flows: flowToSave,
-        rev: revisionCode
-      }
-      this.post('/flow', objectToSave).then(result => {
-        console.log(result);
-      }).catch(err => {
+    this.post('/flow', objectToSave)
+      .then((result) => {
+        return 'success';
+      })
+      .catch((err) => {
         console.log(err);
       });
-    });
-    */
+  }
+
+  private getSwitchConditionCode(
+    conditionCode: string,
+    conditionValue: string,
+  ): any {
+    if (
+      (conditionValue === 'true' || conditionValue === 'false') &&
+      conditionCode === '=='
+    ) {
+      return {
+        t: conditionValue,
+      };
+    } else {
+      const conditionMap = {
+        '<': 'lt',
+        '<=': 'lte',
+        '>': 'gt',
+        '>=': 'gte',
+        '==': 'eq',
+        '!=': 'neq',
+      };
+      return {
+        t: conditionMap[conditionCode],
+        v: conditionValue,
+        vt: 'str', // TODO: A améliorer
+      };
+    }
+  }
+
+  private fillIfThenModel(createScenarioDto: CreateScenarioDTO): NodeRedNode[] {
+    const flow = Object.assign([], NodeRedModels['if-then']);
+    let nextNodeId = this.genId();
+    const flowId = this.genId();
+    for (const [_, rawNode] of flow.entries()) {
+      rawNode.id = nextNodeId;
+      rawNode.z = flowId;
+      nextNodeId = this.genId();
+      if (rawNode.wires.length > 0) {
+        rawNode.wires[0][0] = nextNodeId;
+      }
+    }
+    flow[0].name = createScenarioDto.triggerDevice;
+    flow[0].topic =
+      createScenarioDto.triggerCapability.capabilityData.get.topic;
+    flow[0].broker = MQTT_NODE_ID;
+    if (
+      createScenarioDto.triggerCapability.capabilityData.get.format !== 'json'
+    ) {
+      flow[0].datatype = 'json';
+    }
+
+    flow[1].name =
+      createScenarioDto.triggerCapability.name +
+      createScenarioDto.triggerCondition +
+      createScenarioDto.triggerValue;
+    flow[1].property =
+      'payload.' + createScenarioDto.triggerCapability.capabilityData.get.path;
+    flow[1].rules.push(
+      this.getSwitchConditionCode(
+        createScenarioDto.triggerCondition,
+        createScenarioDto.triggerValue,
+      ),
+    );
+
+    flow[2].name =
+      createScenarioDto.actionCapability.name +
+      ': ' +
+      createScenarioDto.actionValue;
+    flow[2].template =
+      '{\n  "' +
+      createScenarioDto.actionCapability.capabilityData.set.path +
+      '": ' +
+      createScenarioDto.actionValue +
+      '\n}';
+    flow[3].name = createScenarioDto.actionDevice;
+    flow[3].topic = createScenarioDto.actionCapability.capabilityData.set.topic;
+    flow[3].broker = MQTT_NODE_ID;
+    return flow;
   }
 
   public addScenario(createScenarioDto: CreateScenarioDTO) {
     if (createScenarioDto.model === 'if-then') {
-      this.saveFlow("TTT", [
-        {
-          name: createScenarioDto.triggerDevice,
-          type: 'mqtt in',
-          topic: createScenarioDto.triggerCapability.get.topic
-        },
-        {
-          name: 'Action',
-          type: 'template',
-          template: '{\n  "on": false\n}',
-          output: 'json'
-        },
-        {
-          name: createScenarioDto.actionDevice,
-          type: 'mqtt out',
-          topic: createScenarioDto.actionCapability.set.topic
-        },
-      ]);
+      const flow = this.fillIfThenModel(createScenarioDto);
+      this.saveFlow(createScenarioDto.name, flow);
     }
   }
-
 }
